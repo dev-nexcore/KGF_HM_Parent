8824ef7fe1caa47268f7ab686f90bfd5b488a428
Shravv1347 <shettyshravan860@gmail.com>
2025-07-28 10:31:24 +0530

    update page.jsx

--- src/app/dashboard/student/page.jsx
+++ src/app/dashboard/student/page.jsx
@@ -1,5 +1,5 @@
 'use client'
-import Header from '@/components/layout/Header'
+import Navbar from '@/components/layout/navbar'
 import StudentDocuments from '@/components/dashboard/StudentDocuments'
 import Sidebar from '@/components/layout/Sidebar'
 
@@ -8,7 +8,7 @@ export default function StudentPage() {
     <div className="flex">
       <Sidebar />
       <div className="flex-1  min-h-screen">
-        <Header />
+        <Navbar />
        
           <StudentDocuments />
       </div>
