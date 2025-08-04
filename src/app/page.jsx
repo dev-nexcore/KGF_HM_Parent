dbc3d15863a2ecff8e31f93211dad8fdefd88fe9
Resource-02 <resource2.code4bharat@gmail.com>
2025-08-04 21:47:34 +0530

    update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,9 @@
-export const metadata = {
-  title: "Parent Panel",
-  icons: {
-    icon: "/favicon.ico",
-  },
-};
-
 import ParentLogin from "@/components/login/loginpage";
 
-export default function Home() {
-  return (
+export default function Home(){
+  return(
     <>
-      <ParentLogin />
+    <ParentLogin />
     </>
   );
 }
