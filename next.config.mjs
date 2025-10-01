e80751751b25f5f804e72d4bb214534ef24a8596
Akshi1277 <verstapenalfred@gmail.com>
2025-10-01 18:42:36 +0530

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: '/parent',
+  assetPrefix: '/parent/',   // ensures static assets load correctly
+  trailingSlash: true        // helps when deploying in subdirectories
+};
 
 export default nextConfig;
