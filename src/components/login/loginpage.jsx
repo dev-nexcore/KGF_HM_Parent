8b4380d57a74e93bf55302555493d1a480ceaf32
dev-nexcore <nexcorealliance@gmail.com>
2026-03-11 15:09:42 +0530

    Update loginpage.jsx
    updated

--- src/components/login/loginpage.jsx
+++ src/components/login/loginpage.jsx
@@ -549,7 +549,7 @@ export default function ParentLogin() {
           }`}>
             <div className="w-32 h-32 rounded-xl overflow-hidden bg-white shadow-lg">
               <Image 
-                src="/logo2.png"
+                src="/parent/logo2.png"
                 alt="KOKAN Global Foundation Logo" 
                 className="w-full h-full object-cover"
                 width={512}
